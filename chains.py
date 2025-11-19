5f37d8ca6657932023ebad9e3cf52f832d667e91
Alok Ranjan <alokrnjn3@gmail.com>
2025-11-19 14:53:00 +0530

    Refactor model initialization and add chat memory
    Uncommented model initialization and added sql_chat_memory for conversation history.

--- chains.py
+++ chains.py
@@ -2,14 +2,16 @@ from langchain.chains import ConversationalRetrievalChain,LLMChain
 from langchain.memory import ConversationBufferWindowMemory
 from langchain_groq import ChatGroq
 from config import *
+# from config import groq_api_key, model_id
 from prompts import prompt,prompt_llm_chain,prompt_router
 from vectorstore import retriever
 from langchain_core.output_parsers import PydanticOutputParser
+from states import MessageState
 
-model = ChatGroq(api_key=groq_api_key,
-                 model=model_id,
-                 max_tokens=1024,
-                 )
+# model = ChatGroq(api_key=groq_api_key,
+#                  model=model_id,
+#                  max_tokens=1024,
+#                  )
 # chat_memory = ConversationBufferWindowMemory(
 #     memory_key="chat_history",
 #     return_messages=True,
@@ -24,7 +26,7 @@ model = ChatGroq(api_key=groq_api_key,
 #     retriever=retriever
 # )
 
-
+print(groq_api_key)
 model = ChatGroq(api_key=groq_api_key,
                  model=model_id,
                  max_tokens=1024,
@@ -51,3 +53,24 @@ model_chain = LLMChain(
 #     })
 
 router_chain = LLMChain(llm=model, prompt=prompt_router,output_parser=PydanticOutputParser(pydantic_object=MessageState))
+
+sql_chat_memory = ConversationBufferWindowMemory(
+    memory_key="chat_history",
+    return_messages=True,
+    max_tokens=1024,
+    k=5,  # Number of past interactions to keep in memory
+)
+
+# sql_model_chain = ConversationalRetrievalChain.from_llm(
+#     llm=model,
+#     memory=chat_memory,
+#     combine_docs_chain_kwargs={"prompt": prompt},
+#     retriever=sql_retriever
+# )
+
+sql_model_chain = LLMChain(
+    llm=model,
+    memory=sql_chat_memory,
+    prompt=prompt_llm_chain,
+    verbose=True
+)
